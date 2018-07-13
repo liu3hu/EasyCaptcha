ffa80a51dcccbb948a39baa1090225c72f1bcba0
davidliu <liubiao0913@163.com>
2018-07-13 11:19:54 +0800

    u

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-function loadClass_isuydb85df2as74fddf56a2sasf4d7fs($class)
+function loadClass_856bc953c99c81d40b453fa064851218($class)
 {
     if(substr($class, 0, 12) === '\EasyCaptcha'){
         $file = dirname(__DIR__).$class.'.php';
@@ -10,6 +10,6 @@ function loadClass_isuydb85df2as74fddf56a2sasf4d7fs($class)
     }
 }
 $funcs = spl_autoload_functions();
-if(!in_array('loadClass_isuydb85df2as74fddf56a2sasf4d7fs', $funcs)){
-    spl_autoload_register('loadClass_isuydb85df2as74fddf56a2sasf4d7fs');
+if(!in_array('loadClass_856bc953c99c81d40b453fa064851218', $funcs)){
+    spl_autoload_register('loadClass_856bc953c99c81d40b453fa064851218');
 }
